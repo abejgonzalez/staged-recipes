9593120c6296bc4e0716f46be3bd95585cc40df9
Abraham Gonzalez <abe.gonzalez@berkeley.edu>
2022-08-11 16:40:48 +0000

    Temp

--- recipes/ucb-bar-riscv-tools/build.sh
+++ recipes/ucb-bar-riscv-tools/build.sh
@@ -2,4 +2,13 @@
 
 set -ex
 
-NPROC=$CPU_COUNT ./src/build-toolchains.sh --prefix $PREFIX/riscv
+# needed otherwise headers not found
+export LDFLAGS="-Wl,-rpath,$CONDA_PREFIX/lib -Wl,-rpath-link,$CONDA_PREFIX/lib -L$CONDA_PREFIX/lib"
+export CFLAGS="-isystem $CONDA_PREFIX/include"
+export CPPFLAGS="-isystem $CONDA_PREFIX/include"
+export CXXFLAGS="-isystem $CONDA_PREFIX/include"
+export DEBUG_CFLAGS="-isystem $CONDA_PREFIX/include"
+export DEBUG_CPPFLAGS="-isystem $CONDA_PREFIX/include"
+export DEBUG_CXXFLAGS="-isystem $CONDA_PREFIX/include"
+
+NPROC=$CPU_COUNT ./src/build-toolchains.sh --prefix $PREFIX/riscv --ignore-qemu
